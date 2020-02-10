680f8764d3b6f60d982bdcb35b749f88559d5c4e
Keks <keks@htmlacademy.ru>
2020-02-10 17:23:51 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/gallery.js
@@ -0,0 +1,28 @@
+'use strict';
+
+(function () {
+
+  var show = function (mocks) {
+    var containerElement = document.querySelector('.pictures.container');
+    var fragment = document.createDocumentFragment();
+
+    mocks.forEach(function (mock) {
+      fragment.appendChild(window.miniPictureCreate(mock));
+    });
+    containerElement.appendChild(fragment);
+    containerElement.addEventListener('click', onPicturesContainerClick);
+  };
+
+  var onPicturesContainerClick = function (evt) {
+    // если был клик по картинке
+    if (evt.target.classList.contains('picture')
+    || evt.target.matches('.picture [class^="picture__"]')) {
+      // элемент img
+      var pictureClicked = evt.target.closest('.pictures .picture').firstElementChild;
+      window.preview.show(pictureClicked);
+    }
+  };
+
+
+  show(window.mocks);
+})();
---
+++ 6/js/modal.js
@@ -0,0 +1,69 @@
+'use strict';
+
+(function () {
+
+  var modalWindow;
+  var cancelButton;
+  var resetModalWindow;
+
+  var closeModal = function () {
+    if (isModalOpen()) {
+      modalWindow.classList.add('hidden');
+      document.querySelector('body').classList.remove('modal-open');
+      document.removeEventListener('keydown', onModalWindowEscKeydown);
+      cancelButton.removeEventListener('click', onCancelButtonClick);
+      cancelButton.removeEventListener('keydown', onCancelButtonEnterKeydown);
+      resetModalWindow();
+      modalWindow = undefined;
+      cancelButton = undefined;
+      resetModalWindow = undefined;
+    }
+  };
+
+  var onModalWindowEscKeydown = function (evt) {
+    window.util.isEscEvent(evt, closeModal);
+  };
+
+  var onCancelButtonEnterKeydown = function (evt) {
+    window.util.isEnterEvent(evt, closeModal);
+  };
+
+  var onCancelButtonClick = function () {
+    closeModal();
+  };
+
+  var isModalOpen = function () {
+    return document.querySelector('body').classList.contains('modal-open');
+  };
+
+
+  var showModal = function (modal, cancel, callbackReset) {
+    if (!isModalOpen()) {
+      modalWindow = modal;
+      cancelButton = cancel;
+      resetModalWindow = callbackReset;
+
+      document.querySelector('body').classList.add('modal-open');
+      modalWindow.classList.remove('hidden');
+
+      cancelButton.addEventListener('click', onCancelButtonClick);
+      cancelButton.addEventListener('keydown', onCancelButtonEnterKeydown);
+      document.addEventListener('keydown', onModalWindowEscKeydown);
+    }
+  };
+
+  var mustCloseByEsc = function (isEnabled) {
+    if (isEnabled) {
+      document.removeEventListener('keydown', onModalWindowEscKeydown);
+      document.addEventListener('keydown', onModalWindowEscKeydown);
+    } else {
+      document.removeEventListener('keydown', onModalWindowEscKeydown);
+    }
+  };
+
+  window.modal = {
+    show: showModal,
+    mustCloseByEsc: mustCloseByEsc
+  };
+
+})();
